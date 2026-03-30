2f0a2791254570130d041aee9fc92918b923c41b
jihtin <129176693+jihtin@users.noreply.github.com>
2026-03-30 15:22:04 +0530

    feat:update user created fields

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 
 function Home() {
+  const [formInput , setFormInput]= useState({
+    name :"",
+    email :"" , 
+    phone:"" ,
+
+  })
+
+
+
   const navigate = useNavigate();
 
   const user = JSON.parse(localStorage.getItem("user"));
@@ -21,6 +30,11 @@ function Home() {
     navigate("/login");
   };
 
+  const handleChange = (e)=>{
+    const {name ,value} = e.target ;
+    setFormInput({...formInput , [name] : value})
+  }
+
   return (
     <div>
     
@@ -30,6 +44,44 @@ function Home() {
 
       <div>
           <h2>Create User</h2>
+
+          <form   >
+            {/* Name */}
+              <label htmlFor="">Name :</label>
+              <input 
+                type="text"
+                placeholder="Enter username"
+                name="name"
+                value={formInput.name}
+                onChange={handleChange}
+
+                />
+            {/* Email */}
+              <label htmlFor="">Email :</label>
+              <input 
+                type="email"
+                placeholder="Enter email"
+                name="email"
+                value={formInput.email}
+                onChange={handleChange}
+
+                />
+            {/* Phone */}
+              <label htmlFor="">Phone :</label>
+              <input 
+                type="text"
+                placeholder="Enter phone number"
+                name="phone"
+                value={formInput.phone}
+                onChange={handleChange}
+
+                />
+
+                <button type="submit"> Add</button>
+
+
+
+          </form>
       </div>
       
     </div>
